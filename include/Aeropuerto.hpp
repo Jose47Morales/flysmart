f38972583f3b78cdfb30a394397d50616d0346d1
Jose47Morales <josemoralesleon58@gmail.com>
2025-06-20 17:12:24 -0500

    Update commit

--- include/Aeropuerto.hpp
+++ include/Aeropuerto.hpp
@@ -1,1 +1,26 @@
+#ifndef AEROPUERTO_HPP
+#define AEROPUERTO_HPP
 
+#include <string>
+
+class Aeropuerto{
+    private:
+        std::string codigoIATA;
+        std::string nombre;
+        std::string ciudad;
+        std::string pais;
+    
+    public:
+        //Método constructor
+        Aeropuerto(const std::string& codigoIATA, const std::string& nombre, const std::string& ciudad, const std::string& pais);
+
+        //Getters
+        std::string getCodigoIATA() const;
+        std::string getNombre() const;
+        std::string getCiudad() const;
+        std::string getPais() const;
+
+        void mostrarInformacion() const;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include "../include/Aeropuerto.hpp"
 
+int main(){
+    Aeropuerto a1("BOG", "El Dorado", "Bogotá", "Colombia");
+    Aeropuerto a2("MON", "Los Garzones", "Montería", "Colombia");
+    
+    a1.mostrarInformacion();
+    a2.mostrarInformacion();
+
+    return 0;
+}
