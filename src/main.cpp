d4c39232c8e066fad498d87a0f33ca393a54e712
Jose47Morales <josemoralesleon58@gmail.com>
2025-06-24 14:57:13 -0500

    Create vuelos_demo.json and Include json.hpp commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include "../include/json.hpp"
+#include "../include/GrafoDeRutas.hpp"
 #include "../include/Dijkstra.hpp"
+
+using json = nlohmann::json;
+using namespace std;
 
 int main(){
     GrafoDeRutas grafo;
 
-    auto* bog = new Aeropuerto("BOG", "El Dorado", "Bogotá", "Colombia");
-    auto* mon = new Aeropuerto("MON", "Los Garzones", "Montería", "Colombia");
-    auto* mde = new Aeropuerto("MDE", "José María Córdova", "Medellín", "Colombia");
-    auto* ctg = new Aeropuerto("CTG", "Rafael Núñez", "Cartagena", "Colombia");
+    ifstream file("data/vuelos_demo.json");
+    if (!file.is_open()){
+        cerr << "Error al abrir el archivo de vuelos_demo.json\n";
+        return 1;
+    }
 
-    grafo.agregarAeropuerto(bog);
-    grafo.agregarAeropuerto(mon);
-    grafo.agregarAeropuerto(mde);
-    grafo.agregarAeropuerto(ctg);
+    json data;
+    file >> data;
 
-    grafo.agregarVuelo(Vuelo(bog, mde, 350000.0f, 1.2f, 0));
-    grafo.agregarVuelo(Vuelo(mde, ctg, 200000.0f, 1.0f, 0));
-    grafo.agregarVuelo(Vuelo(bog, ctg, 500000.0f, 1.5f, 1));
-    grafo.agregarVuelo(Vuelo(mde, mon, 250000.0f, 1.0f, 0));
-    grafo.agregarVuelo(Vuelo(ctg, mon, 150000.0f, 0.8f, 0));
+    unordered_map<string, Aeropuerto*> mapaAeropuertos;
+
+    for (const auto& a : data["aeropuertos"]) {
+        string cod = a["codigo"];
+        auto* aeropuerto = new Aeropuerto(cod, a["nombre"], a["ciudad"], a["pais"]);
+        mapaAeropuertos[cod] = aeropuerto;
+        grafo.agregarAeropuerto(aeropuerto);
+    }
+
+    for (const auto& v : data["vuelos"]){
+        string origen = v["origen"];
+        string destino = v["destino"];
+        float precio = v["precio"];
+        float duracion = v["duracion"];
+        int escalas = v["escalas"];
+
+        auto* a_origen = mapaAeropuertos[origen];
+        auto* a_destino = mapaAeropuertos[destino];
+
+        grafo.agregarVuelo(Vuelo(a_origen, a_destino, precio, duracion, escalas));
+    }
 
     grafo.mostrarRedDeRutas();
 
-    Dijkstra::encontrarRutaMasCorta(grafo, "BOG", "MON", "precio");
+    string origen, destino, criterio;
+    cout << "\nAeropuerto origen (IATA): ";
+    cin >> origen;
+    cout << "Aeropuerto destino (IATA): ";
+    cin >> destino;
+    cout << "Criterio (precio, duracion, escalas): ";
+    cin >> criterio;
+
+    Dijkstra::encontrarRutaMasCorta(grafo, origen, destino, criterio);
 
     return 0;
 }
